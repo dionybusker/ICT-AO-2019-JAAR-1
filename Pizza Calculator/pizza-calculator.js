8a25ac21ef6307cc2e206411da0a638e6e662e50
dionybusker <99057959@mydavinci.nl>
2019-09-10 21:15:03 +0200

    Pizza Calculator
    + Comments toegevoegd

--- Pizza Calculator/pizza-calculator.js
+++ Pizza Calculator/pizza-calculator.js
@@ -1,26 +1,27 @@
-// begin hier je JavaScript commandos
-
 // Diony Busker
 // LPIAO19A2
 
+// Opdracht: Pizza Calculator
 
-var linebreak = "<br>"
 
-var pizzaSmall = prompt("Hoeveel Small pizza's wenst u?");
+var linebreak = "<br>" // de linebreak zorgt voor een nieuwe regel
 
-var pizzaMedium = prompt("Hoeveel Medium pizza's wenst u?");
+var pizzaSmall = prompt("Hoeveel Small pizza's wenst u?"); // hier wordt een getal ingevuld worden, wordt er 2 ingevuld dan worden er op het eind 2 pizza's verrekend
 
-var pizzaLarge = prompt("Hoeveel Large pizza's wenst u?");
+var pizzaMedium = prompt("Hoeveel Medium pizza's wenst u?"); // hier wordt een getal ingevuld worden, wordt er 2 ingevuld dan worden er op het eind 2 pizza's verrekend
 
-const priceSmall = 8;
-const priceMedium = 10;
-const priceLarge = 12;
+var pizzaLarge = prompt("Hoeveel Large pizza's wenst u?"); // hier wordt een getal ingevuld worden, wordt er 2 ingevuld dan worden er op het eind 2 pizza's verrekend
 
+const priceSmall = 8; // dit is de prijs voor een Small pizza
+const priceMedium = 10; // dit is de prijs voor een Medium pizza
+const priceLarge = 12; // dit is de prijs voor een Large pizza
+
+// na de prompts komt er op het scherm de volgende bestelling te staan (met de eigen ingevulde gegevens)
 document.write("Uw bestelling is als volgt: " + linebreak);
-document.write(pizzaSmall + "×" + " " + "Small pizza" + " " + "(€ " + priceSmall + ")" + linebreak);
-document.write(pizzaMedium + "×" + " " + "Medium pizza" + " " + "(€ " + priceMedium + ")" + linebreak);
-document.write(pizzaLarge + "×" + " " + "Large pizza" + " " + "(€ " + priceLarge + ")" + linebreak);
+document.write(pizzaSmall + "×" + " " + "Small pizza" + " " + "(€ " + priceSmall + " p/s)" + linebreak); // hier wordt de hoeveelheid aan Small pizza's weergegeven, de prijs voor een enkele pizza staat er achter
+document.write(pizzaMedium + "×" + " " + "Medium pizza" + " " + "(€ " + priceMedium + " p/s)" + linebreak); // hier wordt de hoeveelheid aan Medium pizza's weergegeven, de prijs voor een enkele pizza staat er achter
+document.write(pizzaLarge + "×" + " " + "Large pizza" + " " + "(€ " + priceLarge + " p/s)" + linebreak); // hier wordt de hoeveelheid aan Large pizza's weergegeven, de prijs voor een enkele pizza staat er achter
 
-var totalPrice = (pizzaSmall*priceSmall) + (pizzaMedium*priceMedium) + (pizzaLarge*priceLarge)
+var totalPrice = (pizzaSmall*priceSmall) + (pizzaMedium*priceMedium) + (pizzaLarge*priceLarge); // hier wordt de uiteindelijke totaalprijs van alle pizza's verrekend
 
-document.write("Dat maakt dan totaal: € " + totalPrice);
+document.write("Dat maakt dan totaal: € " + totalPrice); // de totaalprijs wordt hiermee op het scherm getoond
