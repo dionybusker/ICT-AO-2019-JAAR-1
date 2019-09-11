3bc90d58f15a9db2bca399907c2caf6b10d63f1c
dionybusker <45560586+dionybusker@users.noreply.github.com>
2019-09-11 09:28:49 +0200

    Add files via upload

---
+++ Labs/Week 2 - Javascript/lab 2/b1w2l2.js
@@ -0,0 +1,16 @@
+// begin hier je JavaScript commandos
+
+// Diony Busker
+// LPIAO19A2
+
+const VRAGEN = alert("U dient 3 vragen te beantwoorden: "); // er wordt een pop up getoond
+
+var voornaam = prompt("Vul uw voornaam in"); // een pop up wordt getoond, hier kan je je naam invullen
+var achternaam = prompt("Vul uw achternaam in"); // een pop up wordt getoond, hier kan je je achternaam invullen
+var leeftijd = prompt("Vul uw leeftijd in"); // een pop up wordt getoond, hier kan je je leeftijd invullen
+
+// na de laatste pop up verschijnen de ingevulde gegevens onder elkaar op het scherm
+document.write("Voornaam: " + voornaam + "<br>" + "Achternaam: " + achternaam + "<br>" + "Leeftijd: " + leeftijd);
+// document.write("Achternaam: " + achternaam + "<br>");
+// document.write("Leeftijd: " + leeftijd);
+
---
+++ Labs/Week 2 - Javascript/lab 3/b1w2l3.js
@@ -0,0 +1,84 @@
+// begin hier je JavaScript commandos
+
+// Diony Busker
+// LPIAO19A2
+
+// // OPDRACHT 1: Maak een Javascript applicatie die de tafel van 6 uitrekent
+// alert("Tafel van 6");
+
+// var tafel = 6;
+// // alert(tafel);
+
+// var antwoord = prompt("Vul hier een getal 1 t/m 10 in.");
+
+// document.write("6 * " + antwoord + " = ")
+
+// document.write(tafel * antwoord);
+
+
+// // OPDRACHT 2: Maak een Javascript applicatie die de tafel van 3 uitrekent
+// alert("Tafel van 3");
+
+// var tafel = 3;
+// // alert(tafel);
+
+// var antwoord = prompt("Vul hier een getal 1 t/m 10 in.");
+
+// document.write("3 * " + antwoord + " = ")
+
+// document.write(tafel * antwoord);
+
+
+// // OPDRACHT 3: Berekeningen uitvoeren
+// // stap 1: + 6
+// // stap 2: * 10
+// // stap 3: / 5
+// // stap 4: - 12
+
+// var begingetal = 12;
+// alert("Begin getal: " + begingetal);
+
+// var plus = begingetal + 6;
+// alert(begingetal + " + " + 6 + " = " +  plus);
+
+// var meervoud = plus * 10;
+// alert(plus + " * " + 10 + " = " + meervoud);
+
+// var delen = meervoud / 5;
+// alert(meervoud + " / " + 5 + " = " + delen);
+
+// var min = delen - 12;
+// alert(delen + " - " + 12 + " = " + min);
+
+// alert("Eind getal: " + min);
+
+
+/* *************************************************************** */
+
+
+// // OPDRACHT 4: Berekeningen uitvoeren met invoer van getal
+// // stap 1: + 6
+// // stap 2: * 10
+// // stap 3: / 5
+// // stap 4: - 12
+
+
+var begin = prompt("Voer een getal in: ");
+var begingetal = parseInt(begin);
+
+alert("Begin getal: " + begingetal);
+
+var result1 = begingetal + 6;
+alert(begingetal + " + " + 6 + " = " + result1);
+
+var result2 = result1 * 10;
+alert(result1 + " * " + 10 + " = " + result2);
+
+var result3 = result2 / 5;
+alert(result2 + " / " + 5 + " = " + result3);
+
+var result4 = result3 - 12;
+alert(result3 + " - " + 12 + " = " + result4);
+
+alert("Eind getal: " + result4);
+
