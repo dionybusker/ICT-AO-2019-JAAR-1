520bb2423f47a5c0464c6098c9c4f39ed590b246
dionybusker <99057959@mydavinci.nl>
2019-09-13 12:03:41 +0200

    Aanpassingen Pizza Calculator

--- Pizza Calculator/js/pizza-calculator.js
+++ Pizza Calculator/js/pizza-calculator.js
@@ -3,39 +3,54 @@
 
 // Opdracht: Pizza Calculator
 
-// function isNumberKey(evt)
-//       {
-//          var charCode = (evt.which) ? evt.which : event.keyCode
-//          if (charCode > 31 && (charCode < 48 || charCode > 57))
-//             return false;
 
-//          return true;
-//       }
+var linebreak = "<br>" // nieuwe regel
+var pizzaSmall;
+var pizzaMedium;
+var pizzaLarge;
+var valid = false; // valid zal standaard false zijn
 
-// de linebreak zorgt voor een nieuwe regel
-var linebreak = "<br>"
+while (!valid) {
+    pizzaSmall=parseInt(prompt("Hoeveel Small pizza's wenst u?"));
+    if (Number.isNaN(pizzaSmall)) {
+        alert("Gelieve een nummer in te voeren.");
+    } else {
+        valid = true;
+    }
+}
 
-// diverse pizza formaten waar uit gekozen kan worden
-var pizzaSmall = prompt("Hoeveel Small pizza's wenst u?") || 0 ;
-var pizzaMedium = prompt("Hoeveel Medium pizza's wenst u?") || 0;
-var pizzaLarge = prompt("Hoeveel Large pizza's wenst u?") || 0;
+valid = false // valid wordt weer veranderd naar false
+while (!valid) {
+    pizzaMedium=parseInt(prompt("Hoeveel Medium pizza's wenst u?"));
+    if (Number.isNaN(pizzaMedium)) {
+        alert("Gelieve een nummer in te voeren.");
+    } else {
+        valid = true;
+    }
+}
+
+valid = false // valid wordt weer veranderd naar false
+while (!valid) {
+    pizzaLarge=parseInt(prompt("Hoeveel Large pizza's wenst u?"));
+    if (Number.isNaN(pizzaLarge)) {
+        alert("Gelieve een nummer in te voeren.");
+    } else {
+        valid = true;
+    }
+}
 
 // standaard prijzen voor de diverse formaten pizza's
 const PRICE_SMALL = 7.95;
 const PRICE_MEDIUM = 9.95;
 const PRICE_LARGE = 11.95;
 
-
 // na de prompts komt er op het scherm de volgende bestelling te staan (met de eigen ingevulde gegevens)
 document.write("Uw bestelling is als volgt: " + linebreak + linebreak);
 var priceSmall = parseFloat(pizzaSmall * PRICE_SMALL).toFixed(2);
-// priceSmall.toFixed(2);
 document.write("<strong>" + pizzaSmall + "× </strong>" + " " + "Small pizza" + " " + "(€ " + PRICE_SMALL + " p/s, totaal € " + priceSmall + ")" + linebreak);
 var priceMedium = parseFloat(pizzaMedium * PRICE_MEDIUM).toFixed(2);
-// priceMedium.toFixed(2);
 document.write("<strong>" + pizzaMedium + "× </strong>" + " " + "Medium pizza" + " " + "(€ " + PRICE_MEDIUM + " p/s, totaal € " + priceMedium + ")" + linebreak);
 var priceLarge = parseFloat(pizzaLarge * PRICE_LARGE).toFixed(2);
-// priceLarge.toFixed(2);
 document.write("<strong>" + pizzaLarge + "× </strong>" + " " + "Large pizza" + " " + "(€ " + PRICE_LARGE + " p/s, totaal € " + priceLarge + ")" + linebreak);
 
 // hier wordt de uiteindelijke totaalprijs van alle pizza's verrekend
