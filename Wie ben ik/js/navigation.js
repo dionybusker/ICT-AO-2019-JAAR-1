a9740f71a190cb04359431ddc812000af9217f67
dionybusker <99057959@mydavinci.nl>
2019-09-20 08:24:31 +0200

    Opdracht: wie ben ik
    Aanpassingen en toevoegingen in de html en styling, nieuwe pagina's toegevoegd, nav dmv javascript eenmalig aan te passen (te zien in alle documenten) (donderdag 19-9-2019)

---
+++ Wie ben ik/js/navigation.js
@@ -0,0 +1,21 @@
+document.write('<img class="logo" src="img/windowsxp.png" alt="logo">\
+    <nav id="navbar">\
+    <ul>\
+        <li><a href="index.html">Home</a></li>\
+        <li><a href="about.html">Over mij</a></li>\
+        <li class="dropdown">\
+            <a href="" class="dropbtn">Opleiding</a>\
+            <div class="dropdown-content">\
+                <a href="hbo_verpleegkunde.html">hbo Verpleegkunde</a>\
+                <a href="hbo_informatica.html">hbo Informatica</a>\
+                <a href="mbo_applicatie_ontwikkeling.html">mbo Applicatie Ontwikkeling</a>\
+            </div>\
+        </li>\
+        <li><a  href="werk.html">Werk</a></li>\
+        <li><a href="vaardigheden.html">Vaardigheden</a></li>\
+        <li><a href="ontwikkeling.html">Ontwikkelingen</a></li>\
+        <li><a  href="portfolio.html">Portfolio</a></li>\
+        <li style="float: right;"><a href="contact.html">Contact</a></li>\
+    </ul>\
+    </nav>\
+');
