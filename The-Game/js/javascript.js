db16d2c74643202ba29aaf60881d39fbf59d7745
dionybusker <99057959@mydavinci.nl>
2019-09-25 23:07:50 +0200

    Toevoegingen
    events.js : events aangepast en nieuwe toegevoegd
    options.js : options aangepast en nieuwe toegevoegd waar nodig 
    javascript.js : nested if else statements gemaakt met de events en options in alerts en prompts

--- The-Game/js/javascript.js
+++ The-Game/js/javascript.js
@@ -1,1 +1,91 @@
-// javascript.js
+var error = "Dat is geen geldig antwoord. Om de optie die je wilt doen te kiezen, dan graag een '1' of een '2' invullen.";
+var lose = "Jammer! Je bent game over!";
+var win = "Gefeliciteerd! Je hebt de game gewonnen!"
+
+alert("dit is een game");
+
+alert(event1)
+var event1response = prompt(event1option1 + "\n" + event1option2); // "\n" zorgt voor een nieuwe regel
+if (event1response == '1') { // keuze van speler is optie 1
+    console.log("Level 1");
+    alert(event2);
+    alert(lose); // je gaat hier game over
+} else if (event1response == '2') { // keuze van speler is optie 2
+    console.log("Level 2");
+    alert(event3);
+    var event3response = prompt(event3option1 + "\n" + event3option2);
+    if (event3response == '1') { // keuze van speler is optie 1
+        alert(event4);
+        var event4response = prompt(event4option1 + "\n" + event4option2);
+        if (event4response == '1') { // keuze van speler is optie 1
+            alert(event5);
+            alert(lose); // je gaat hier game over
+        } else if (event4response == '2') { // keuze van speler is optie 2
+            alert(event7);
+            alert(lose); // je gaat hier game over
+        } else {
+            alert(error);
+        }
+    } else if (event3response == '2') { // keuze van speler is optie 2
+    alert(event6);
+        var event6response = prompt(event6option1 + "\n" + event6option2); // "\n" zorgt voor een nieuwe regel
+        if (event6response == '1') { // keuze van speler is optie 1
+            alert(event10);
+            var event10response = prompt(event10option1 + "\n" + event10option2); // "\n" zorgt voor een nieuwe regel
+            if (event10response == '1') { // keuze van speler is optie 1
+                alert(event11);
+                alert(lose); // je gaat hier game over
+            } else if (event10response == "2") { // keuze van speler is optie 1
+                alert(event8);
+                alert(lose); // je gaat hier game over
+            } else {
+                alert(error);
+            }
+        } else if (event6response == '2') { // keuze van speler is optie 2
+            alert(event12);
+            var event12response = prompt(event12option1 + "\n" + event12option2);
+            if (event12response == '1') { // keuze van speler is optie 1
+                alert(event13);
+                alert(lose); // je gaat hier game over
+            } else if (event12response == '2') {
+                alert(event14);
+                var event14response = prompt(event14option1 + "\n" + event14option2);
+                if (event14response == '1') {
+                    alert(event15);
+                    var event15response = prompt(event15option1 + "\n" + event15option2);
+                    if (event15response == '1') {
+                        alert(event16);
+                        alert(win); // hier win je
+                    } else if (event15response == '2') {
+                        alert(event17);
+                        var event17response = prompt(event17option1 + "\n" + event17option2);
+                        if (event17response == '1') {
+                            alert(event18);
+                            alert(lose); // je gaat hier game over
+                        } else if (event17response == '2') {
+                            alert(event19);
+                            alert(lose); // je gaat hier game over
+                        } else {
+                            alert(error);
+                        }
+                    } else {
+                        alert(error);
+                    }
+                } else if (event14response == '2') {
+                    alert(event17);
+                    alert(lose); // je gaat hier game over
+                } else {
+                    alert(error);
+                }
+            } else {
+                alert(error);
+            }
+        } else {
+            alert(error);
+        }
+    } else {
+        alert(error); // er is een ongeldig antwoord gegeven, of er is op enter gedrukt
+    }
+} else {
+    alert(error); // er is een ongeldig antwoord gegeven, of er is op enter gedrukt
+}
--- The-Game/js/options.js
+++ The-Game/js/options.js
@@ -1,1 +1,26 @@
-// options.js
+var event1option1 = "Optie 1: je blijft zitten en wacht het uit."; // option1 op event1
+var event1option2 = "Optie 2: je staat op en gaat op onderzoek uit."; // option2 op event1
+
+var event3option1 = "Optie 1: je gaat naar links"; // option1 op event3
+var event3option2 = "Optie 2: je gaat naar rechts"; // option2 op event3
+
+var event4option1 = "Optie 1: je opent de deur en loopt de ruimte binnen"; // option1 op event4
+var event4option2 = "Optie 2: je loopt terug naar de andere kant van de gang"; // option2 op event 4
+
+var event6option1 = "Optie 1: je opent de lade en bekijkt de mappen"; // option1 op event6
+var event6option2 = "Optie 2: je gaat verder door de volgende deur" // option2 op event6
+
+var event10option1 = "Optie 1: je besluit de map te bekijken"; // option1 op event10
+var event10option2 = "Optie 2: je laat de map liggen en loopt toch naar de deur"; // option2 op event10
+
+var event11option1 = "Optie 1: je blijft staan en wacht het af" // option1 op event11
+var event11option2 = "Optie 2: je verstopt je toch in de kast" // option2 op event11
+
+var event12option1 = "Optie 1: je zet je telefoon uit"; // option1 op event12
+var event12option2 = "Optie 2: je neemt op en hoort zacht geruis"; // option2 op event12
+
+var event13option1 = "Optie 1: je kruipt door de kruipruimte"; // option1 op event13
+var event13option2 = "Optie 2: je vertrouwt het niet en je besluit in de huidige ruimte te blijven"; // option2 op event13
+
+var event15option1 = "Optie 1: je staat op en pakt de mondkap van de kast"; // option1 op event15
+var event15option2 = "Optie 2: je blijft lekker zitten en knijpt je neus maar dicht"; // option2 op event15
