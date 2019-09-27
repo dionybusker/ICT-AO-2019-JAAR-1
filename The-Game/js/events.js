544dea27832118353a44c874f6d73f528174c289
dionybusker <99057959@mydavinci.nl>
2019-09-27 08:42:16 +0200

    comments
    comments toegevoegd en ; toegevoegd waar nodig

--- The-Game/js/events.js
+++ The-Game/js/events.js
@@ -12,26 +12,26 @@ var event6 = "Je loopt door de donkere gang tot je aan het eind voor een open de
 
 var event7 = "Zodra je terug wilt lopen, slaat plots de deur naast je open. Twee mannen komen tevoorschijn. Je bent gepakt."; // vervolg op event4option2
 
-var event8 = "Direct wanneer je je omdraait, zie je een gestalte voor je staan. Je probeert deze persoon te ontwijken, echter ben je niet snel genoeg en word je alsnog gepakt."; // vervolg op event10option2
+var event8 = "Direct wanneer je je omdraait, zie je een gestalte voor je staan. Je probeert deze persoon te ontwijken, echter ben je niet snel genoeg en word je alsnog gepakt."; // vervolg op event9option2
 
 var event9 = "Je bekijkt de mappen en ziet je naam ergens bovenaan staan. Dat triggert je nieuwsgierigheid. Ga je de map bekijken, of laat je het liggen en loop je toch naar de deur?"; // vervolg op event6option1
 
-var event10 = "Je bekijkt de map en ziet dat er allemaal onderzoeken zijn gedaan. Achter je hoor je iemand dichterbij komen. Je voelt een klap tegen je hoofd en alles wordt zwart."; // vervolg op event7option1
+var event10 = "Je bekijkt de map en ziet dat er allemaal onderzoeken zijn gedaan. Achter je hoor je iemand dichterbij komen. Je voelt een klap tegen je hoofd en alles wordt zwart."; // vervolg op event9option1
 
-var event11 = "Je komt een nieuwe ruimte binnen. Achter je hoor je de vorige deur piepend open gaan. Er volgen voetstappen. Je kijkt om je heen en ziet een kast. Echter ziet deze er vrij krap uit. Je weet niet of je er in past. Blijf je staan en wacht je het af tot de voetstappen verdwijnen, of verstop je je toch in de kast? Wat doe je?"; // vervolg op event6option2 & vervolg op event7option2
+var event11 = "Je komt een nieuwe ruimte binnen. Achter je hoor je de vorige deur piepend open gaan. Er volgen voetstappen. Je kijkt om je heen en ziet een kast. Echter ziet deze er vrij krap uit. Je weet niet of je er in past. Blijf je staan en wacht je het af tot de voetstappen verdwijnen, of verstop je je toch in de kast? Wat doe je?"; // vervolg op event6option2
 
-var event12 = "De voetstappen komen dichterbij en de deur achter je gaat open. Je wordt vastgegrepen en meegenomen."; // vervolg op event9option1
+var event12 = "De voetstappen komen dichterbij en de deur achter je gaat open. Je wordt vastgegrepen en meegenomen."; // vervolg op event11option1
 
-var event13 = "Met moeite je je in de kast weten te proppen. De deur van de ruimte gaat open en de voetstappen komen dichterbij. Je houdt je adem in en wacht tot de persoon de ruimte uit loopt. De deur sluit en je telefoon gaat plotseling af. Je kijkt wie je belt en ziet dat het je vader is. Je twijfelt om hem uit te zetten of om hem op te nemen. Wat doe je?"; // vervolg op event9option2
+var event13 = "Met moeite je je in de kast weten te proppen. De deur van de ruimte gaat open en de voetstappen komen dichterbij. Je houdt je adem in en wacht tot de persoon de ruimte uit loopt. De deur sluit en je telefoon gaat plotseling af. Je kijkt wie je belt en ziet dat het je vader is. Je twijfelt om hem uit te zetten of om hem op te nemen. Wat doe je?"; // vervolg op event11option2
 
-var event14 = "Je zet je telefoon uit en stopt hem terug in je broekzak. Je doet de kastdeur een stukje open en spiekt naar buiten. Er staat niemand in de ruimte. Je ziet een rooster en loopt ernaartoe. Het rooster valt zodra je het een zetje geeft. Nu kan je erdoorheen kruipen om te zien wat er zich aan de andere kant bevindt, of je vertrouwt het toch niet en blijft waar je bent om een andere uitweg te vinden. Wat doe je?"; // vervolg op event11option1
+var event14 = "Je zet je telefoon uit en stopt hem terug in je broekzak. Je doet de kastdeur een stukje open en spiekt naar buiten. Er staat niemand in de ruimte. Je ziet een rooster en loopt ernaartoe. Het rooster valt zodra je het een zetje geeft. Nu kan je erdoorheen kruipen om te zien wat er zich aan de andere kant bevindt, of je vertrouwt het toch niet en blijft waar je bent om een andere uitweg te vinden. Wat doe je?"; // vervolg op event13option1
 
-var event15 = "Aan het eind van de kruipruimte zie je licht. Je duwt het rooster voor je open en valt met je gezicht in het gras. Je beseft dat je zojuist de uitgang gevonden hebt. Je bent vrij!"; // vervolg op event12option1
+var event15 = "Aan het eind van de kruipruimte zie je licht. Je duwt het rooster voor je open en valt met je gezicht in het gras. Je beseft dat je zojuist de uitgang gevonden hebt. Je bent vrij!"; // vervolg op event14option1
 
-var event16 = "Terwijl je op de grond zit na te denken wat je nu moet doen, begint het ineens vreemd te ruiken. Je ziet een mondkap liggen op een kast, maar die kast staat nog best ver van je vandaan. Je besluit op te staan en de mondkap te pakken, of je hebt absoluut geen zin om op te staan omdat je net zo lekker zit dus knijp je maar je neus dicht. Wat doe je?"; // vervolg op event12option2
+var event16 = "Terwijl je op de grond zit na te denken wat je nu moet doen, begint het ineens vreemd te ruiken. Je ziet een mondkap liggen op een kast, maar die kast staat nog best ver van je vandaan. Je besluit op te staan en de mondkap te pakken, of je hebt absoluut geen zin om op te staan omdat je net zo lekker zit dus knijp je maar je neus dicht. Wat doe je?"; // vervolg op event14option2
 
-var event17 = "De mondkap was gescheurd. Je gaat dood door de giftige stof in de ruimte die de vreemde geur veroorzaakte."; // vervolg op event14option1
+var event17 = "De mondkap was gescheurd. Je gaat dood door de giftige stof in de ruimte die de vreemde geur veroorzaakte."; // vervolg op event16option1
 
-var event18 = "Je gaat dood omdat je vergeten bent door je mond te ademen."; // vervolg op event14option2
+var event18 = "Je gaat dood omdat je vergeten bent door je mond te ademen."; // vervolg op event16option2
 
-var event19 = "Je fluistert in de speaker en hoopt dat je vader je hoort. Dat is alleen niet het geval. Broekzak bellen... Je praat iets harder en ziet dan ineens de kastdeur voor je opengaan. Twee mannen staan vlak voor je neus en trekken je uit de kast."; // vervolg op event11option2
+var event19 = "Je fluistert in de speaker en hoopt dat je vader je hoort. Dat is alleen niet het geval. Broekzak bellen... Je praat iets harder en ziet dan ineens de kastdeur voor je opengaan. Twee mannen staan vlak voor je neus en trekken je uit de kast."; // vervolg op event13option2
--- The-Game/js/options.js
+++ The-Game/js/options.js
@@ -8,19 +8,19 @@ var event4option1 = "Optie 1: je opent de deur en loopt de ruimte binnen"; // op
 var event4option2 = "Optie 2: je loopt terug naar de andere kant van de gang"; // option2 op event 4
 
 var event6option1 = "Optie 1: je opent de lade en bekijkt de mappen"; // option1 op event6
-var event6option2 = "Optie 2: je gaat verder door de volgende deur" // option2 op event6
+var event6option2 = "Optie 2: je gaat verder door de volgende deur"; // option2 op event6
 
 var event9option1 = "Optie 1: je besluit de map te bekijken"; // option1 op event9
 var event9option2 = "Optie 2: je laat de map liggen en loopt toch naar de deur"; // option2 op event9
 
-var event11option1 = "Optie 1: je blijft staan en wacht het af" // option1 op event11
-var event11option2 = "Optie 2: je verstopt je toch in de kast" // option2 op event11
+var event11option1 = "Optie 1: je blijft staan en wacht het af"; // option1 op event11
+var event11option2 = "Optie 2: je verstopt je toch in de kast"; // option2 op event11
 
-var event13option1 = "Optie 1: je zet je telefoon uit"; // option1 op event12
-var event13option2 = "Optie 2: je neemt op en hoort zacht geruis"; // option2 op event12
+var event13option1 = "Optie 1: je zet je telefoon uit"; // option1 op event13
+var event13option2 = "Optie 2: je neemt op en hoort zacht geruis"; // option2 op event13
 
-var event14option1 = "Optie 1: je kruipt door de kruipruimte"; // option1 op event13
-var event14option2 = "Optie 2: je vertrouwt het niet en je besluit in de huidige ruimte te blijven"; // option2 op event13
+var event14option1 = "Optie 1: je kruipt door de kruipruimte"; // option1 op event14
+var event14option2 = "Optie 2: je vertrouwt het niet en je besluit in de huidige ruimte te blijven"; // option2 op event14
 
-var event16option1 = "Optie 1: je staat op en pakt de mondkap van de kast"; // option1 op event15
-var event16option2 = "Optie 2: je blijft lekker zitten en knijpt je neus maar dicht"; // option2 op event15
+var event16option1 = "Optie 1: je staat op en pakt de mondkap van de kast"; // option1 op event16
+var event16option2 = "Optie 2: je blijft lekker zitten en knijpt je neus maar dicht"; // option2 op event16
